eacf08b6dd737c00fe517b5fe4b5e8233187ecce
Bohdan Savchuk <leonidovich1804@gmail.com>
2020-02-19 21:34:57 +0200

    task hard

--- script/sript.js
+++ script/sript.js
@@ -112,4 +112,29 @@ window.addEventListener('DOMContentLoaded', function(){
 
     togglePopUp();
 
+
+    // scroll
+
+    const menuLinks = document.querySelectorAll('menu > ul > li > a'),
+            mainBtn = document.querySelector('[href="#service-block"]');        
+
+    const scroll = (elem) => {
+        elem.addEventListener('click', function (event) {
+            event.preventDefault();
+            
+            const blockID = elem.getAttribute('href');
+
+            document.querySelector(blockID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    };
+
+    menuLinks.forEach((item) => {
+        scroll(item);
+    });
+
+    scroll(mainBtn);
+
 });  
