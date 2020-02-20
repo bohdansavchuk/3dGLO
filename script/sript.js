0e848d25220052f27df36d228fe5dad596c8b5e6
Bohdan Savchuk <leonidovich1804@gmail.com>
2020-02-21 01:28:45 +0200

    task to lesson19

--- script/sript.js
+++ script/sript.js
@@ -1,68 +1,82 @@
 window.addEventListener('DOMContentLoaded', function(){
     'use strict';
 
-    //таймер
+    // timer
     
-    let date;
-    date = new Date();
-    date.setDate(date.getDate() +1);
-    date.setHours(0, 0, 0); 
-
-    function countTimer() {
-
-        let timerHours = document.getElementById('timer-hours'),
-            timerMinutes = document.getElementById('timer-minutes'),
-            timerSeconds = document.getElementById('timer-seconds');
-
-        function getTimeRemaining(){
-            let dateStop = date.getTime(),
-                dateNow = new Date().getTime(),
-                timeRemaining = (dateStop - dateNow) / 1000,
-                seconds = Math.floor((timeRemaining % 60)),
-                minutes = Math.floor((timeRemaining / 60) % 60),
-                hours = Math.floor(timeRemaining / 60 / 60);
-                return {timeRemaining, seconds, minutes, hours};
-        }
-
-        function updateClock() {
-            let timer = getTimeRemaining();
-            
-            timerHours.textContent = ("0" + timer.hours).slice(-2);
-            timerMinutes.textContent = ("0" + timer.minutes).slice(-2);
-            timerSeconds.textContent = ("0" + timer.seconds).slice(-2);
-
-            if(timer.timeRemaining < 1){
-
-                date.setDate(date.getDate() +1);
+    const getTimer = () => {
+        let date;
+        date = new Date();
+        date.setDate(date.getDate() +1);
+        date.setHours(0, 0, 0); 
+    
+        function countTimer() {
+    
+            let timerHours = document.getElementById('timer-hours'),
+                timerMinutes = document.getElementById('timer-minutes'),
+                timerSeconds = document.getElementById('timer-seconds');
+    
+            function getTimeRemaining(){
+                let dateStop = date.getTime(),
+                    dateNow = new Date().getTime(),
+                    timeRemaining = (dateStop - dateNow) / 1000,
+                    seconds = Math.floor((timeRemaining % 60)),
+                    minutes = Math.floor((timeRemaining / 60) % 60),
+                    hours = Math.floor(timeRemaining / 60 / 60);
+                    return {timeRemaining, seconds, minutes, hours};
+            }
+    
+            function updateClock() {
+                let timer = getTimeRemaining();
+                
+                timerHours.textContent = ("0" + timer.hours).slice(-2);
+                timerMinutes.textContent = ("0" + timer.minutes).slice(-2);
+                timerSeconds.textContent = ("0" + timer.seconds).slice(-2);
+    
+                if(timer.timeRemaining < 1){
+    
+                    date.setDate(date.getDate() +1);
+                } 
             } 
+    
+            updateClock();
+    
+            setInterval(updateClock, 1000);
+    
         } 
+    
+        countTimer();
+    };
 
-        updateClock();
+    getTimer();
 
-        setInterval(updateClock, 1000);
-
-    } 
-
-    countTimer();
-
-    //меню
+    // menu
 
     const toggleMenu = () => {
 
-        const btnMenu = document.querySelector('.menu'),
-            menu = document.querySelector('menu'),
-            closeBtn = document.querySelector('.close-btn'),
-            menuItems = menu.querySelectorAll('ul>li');
+        const menu = document.querySelector('menu'),
+            mainHeader = document.querySelector('.main-header');
 
         const handlerMenu = () => {
             menu.classList.toggle('active-menu');
         };
 
-        btnMenu.addEventListener('click', handlerMenu);   
+        mainHeader.addEventListener('click', (event) => {
+            let target = event.target;
+            target = target.closest('.menu');
+    
+            if(target) {
+                handlerMenu();
+            }
+        });   
 
-        closeBtn.addEventListener('click', handlerMenu); 
+        menu.addEventListener('click', (event) => {
+           let target = event.target;
 
-        menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
+           if(target.tagName === 'A') {
+                handlerMenu();
+           } 
+
+        });
 
     };
 
@@ -73,7 +87,6 @@ window.addEventListener('DOMContentLoaded', function(){
     const togglePopUp = () => { 
         const popup = document.querySelector('.popup'),
             popupBtn = document.querySelectorAll('.popup-btn'),
-            popupClose = document.querySelector('.popup-close'),
             popupContent = document.querySelector('.popup-content');
 
         let count = 0,
@@ -101,11 +114,25 @@ window.addEventListener('DOMContentLoaded', function(){
             
         });
 
-        popupClose.addEventListener('click', () => {
-            cancelAnimationFrame(animate);
-            popupContent.style.top = 10 + '%';
-            popup.style.display = 'none';
-            count = 0;
+        popup.addEventListener('click', (event) => {
+            let target = event.target;
+
+            if(target.classList.contains('popup-close')) {
+                cancelAnimationFrame(animate);
+                popupContent.style.top = 10 + '%';
+                popup.style.display = 'none';
+                count = 0;
+            } else {
+                target = target.closest('.popup-content');
+
+                if(!target) {
+                    cancelAnimationFrame(animate);
+                    popupContent.style.top = 10 + '%';
+                    popup.style.display = "none";
+                    count = 0;
+                }
+            }
+            
         });
         
     };
@@ -115,7 +142,7 @@ window.addEventListener('DOMContentLoaded', function(){
 
     // scroll
 
-   const getScroll = function(){
+   const getScroll =() => {
 
     const menuLinks = document.querySelectorAll('menu > ul > li > a'),
     mainBtn = document.querySelector('[href="#service-block"]');        
@@ -142,5 +169,40 @@ window.addEventListener('DOMContentLoaded', function(){
    };
 
    getScroll();
+
+   // tabs
+
+    const tabs = () => {
+        const tabHeader = document.querySelector('.service-header'),
+            tab = tabHeader.querySelectorAll('.service-header-tab'),
+            tabContent = document.querySelectorAll('.service-tab');
+
+            const toggleTabContent = (index) => {
+                for (let i = 0; i < tabContent.length; i++) {
+                    if(index === i) {
+                        tab[i].classList.add('active');
+                        tabContent[i].classList.remove('d-none');
+                    } else {
+                        tab[i].classList.remove('active');
+                        tabContent[i].classList.add('d-none');
+                    }
+                }
+            };
+
+            tabHeader.addEventListener('click', (event) => {
+                let target = event.target;
+                    target = target.closest('.service-header-tab');
+
+                if(target) {
+                    tab.forEach((item, i) => {
+                        if(item === target) {
+                            toggleTabContent(i);
+                        }
+                    });
+                }
+            });
+    };
+
+   tabs();
 
 });  
